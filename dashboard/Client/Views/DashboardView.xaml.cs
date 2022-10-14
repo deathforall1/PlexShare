700bb72ad797431b779bae3a52e85aa98430b4fa
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2022-10-14 18:34:27 +0530

    Made the basic template for showing the participants list. Moving to create the binding for this purpose

--- dashboard/Client/Views/DashboardView.xaml.cs
+++ dashboard/Client/Views/DashboardView.xaml.cs
@@ -23,11 +23,7 @@ namespace Client.Views
         public DashboardView()
         {
             InitializeComponent();
-            List<User> items = new List<User>();
-            items.Add(new User() { Name = "John Doe", Age = 42, Mail = "john@doe-family.com" });
-            items.Add(new User() { Name = "Jane Doe", Age = 39, Mail = "jane@doe-family.com" });
-            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
-            lvDataBinding.ItemsSource = items;
+            
 
         }
     }
