6d8532ad1477e6a5362cc521ea6e5b8be071e6c6
Saurabh <saurabhkumar121201@gmail.com>
2022-10-17 21:40:21 +0530

    added Meeting Credential class

---
+++ src/PlexShare/Dashboard/MeetingCredentials.cs
@@ -0,0 +1,22 @@
+﻿// defining the datastructure for the Meeting Credential
+
+namespace Dashboard
+{
+    public class MeetingCredentials
+    {
+        public string ipAddress;
+        public int port;
+
+        /// <summary>
+        ///     Instances of this class will store the
+        ///     credentrials required to join/start
+        /// </summary>
+        /// <param name="address"> String parameter to store the IP Address </param>
+        /// <param name="portNumber"> Int parameter for the port number </param>
+        public MeetingCredentials(string address, int portNumber)
+        {
+            ipAddress = address;
+            port = portNumber;
+        }
+    }
+}
