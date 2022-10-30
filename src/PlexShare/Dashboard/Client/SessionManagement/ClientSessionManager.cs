b2bd56aaba18eb38f84fd8435bde6e0085a5b130
Saurabh <saurabhkumar121201@gmail.com>
2022-10-30 19:15:32 +0530

    created session management for client

---
+++ src/PlexShare/Dashboard/Client/SessionManagement/ClientSessionManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Content;
+using Dashboard.Server.Telemetry;
+using Networking;
+using ScreenSharing;
+using Whiteboard;
+
+namespace Dashboard.Client.SessionManagement
+{
+    public delegate void NotifyEndMeet();
+
+    public delegate void NotifyAnalyticsCreated(SessionAnalytics analytics);
+
+    public delegate void NotifySummaryCreated(string summary);
+
+    //     ClientSessionManager class is used to maintain the client side
+    //     session data and requests from the user. It communicates to the server session manager
+    //     to update the current session or to fetch summary and analytics.
+    public class ClientSessionManager : IUXClientSessionManager, INotificationHandler
+    {
+
+    }
+}
